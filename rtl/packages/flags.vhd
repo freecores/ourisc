8fff750b7bd4ac5089717bcb91a46468285d74ca
joaocarlos <joaocarlos@5e8402ae-e35f-466a-a9f7-4296efaf20c9>
2013-08-01 01:51:53 +0000

    uRISC predefined processor flags.

--- rtl/packages/flags.vhd
+++ rtl/packages/flags.vhd
@@ -7,12 +7,12 @@
 --   To use any of the example code shown below, uncomment the lines and modify as necessary
 --
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
-package Flags is
+package flags is
 	constant equals :	std_logic_vector (3 downto 0) := "0001";
 	constant above :	std_logic_vector (3 downto 0) := "0010";
 	constant overflow :	std_logic_vector (3 downto 0) := "0100";
 	constant error :	std_logic_vector (3 downto 0) := "1000";
-end Flags;
+end flags;
